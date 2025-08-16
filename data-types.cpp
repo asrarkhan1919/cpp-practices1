d86ac5f9d104abd0b3eb430ff6fefe980a9e9d82
asrarkhan1919 <mirhadiak47@gmail.com>
2025-08-17 00:24:17 +0430

    data types practiced

---
+++ data-types.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+#define length 10
+main(){
+	
+//	string s="all list of your salary";
+//	int ahsan=3000; 
+//	int irfan=2000;
+//	float asrar=2000.5;
+//	char d='a';
+//	
+//	int sabawoon=ahsan+irfan;
+//	bool e= true;
+//	
+//    cout <<s<<endl;	
+//	cout <<"ehsan: "<<ahsan<<endl;
+//	cout <<"irfan: "<<irfan<<endl;
+//	cout <<"asrar: "<<asrar<<endl;
+//	cout <<"sabawoon: "<<sabawoon<<endl;
+//	cout <<d;
+//	
+//	string logo ;
+//	string  video;
+//	int image=40;
+//	cout<<"enter your logo ty: ";
+//	cin>>logo;
+//	cout<<"enter your vedo ty: ";
+//	cin>>video; 
+//	cout<<"enter your image ty : ";
+//	cin>>image;
+	//	cout<<"\ninter your id no: "<<yourid<<"\nyour phone no: "<<yourno<<"\nyour balance: "<<now;
+	
+/*	
+	string w="welcome Mr or Mrs";
+	float apple;
+	float banana;
+	float orange;
+	float grips;
+	
+	
+	cout<<"enter your cash";
+	cin>>en;
+	cout<<w<<endl;
+	cout<<"apple"<<endl;
+	cin>>apple;
+	cout<<"banana"<<endl;
+	cin >>banana;
+	cout<<"orange"<<endl;
+	cin>> orange;
+	cout<<"grips"<<endl;
+	cin>> grips;
+
+
+*/
+
+// float a=10;
+// float b=20;
+// float c=5;
+// //operator presedence 1(perenthesis) 2 (division,multiplication) 3 (subtraction, addition) 
+// double result= a + b  * c; //110  10+ 20* 5==> 10+ 100=>110 
+//// cout <<result;
+// result > 140 ? cout<<"result is greater than 140" : cout<<"result is less than 140";
+//// bool result= a!=111;
+////cout<< &a ;
+////cout<<&result;
+
+
+
+
+
+
+//    cout<<"welcome MR or MRS"<<endl;
+//    
+//    string name ;
+//    string fname;
+//    int   cash;
+//    
+//    
+//    cout<<"HI what is your name"<<endl;
+//    cin>>name;
+//    cout<<"what is your fname"<<endl;
+//    cin>>fname;
+//    cout<<"enter your all cash"<<endl;
+//    cin >>cash;
+//    if(cash<=100){
+//    	
+//    	cout<<"sorry you cant buy anything";
+//	}
+//	if(cash>=100){
+//		
+//	cout<<"welcom what you want to buy here "<<endl;
+//	}
+// if(cash>=100){
+//	
+//	
+//   cout<<" apples 20RS  oranges  30RS  mangoes 70RS "<<endl;
+//	
+//}
+//else if (cash>=70){
+//	cout <<""
+//}
+
+
+	int a=20;
+	
+	if(a==30){
+		
+		cout<<"this is 30"<<endl;
+	}
+	 if (a==20){
+		cout<<"this is 2nd 20 "<<endl;
+	}
+	 if (a==30){
+		cout<<"this is 30"<<endl;
+	}
+	 if  (a==50){
+		cout<<"this is 50"<<endl;
+	}
+
+}
+  
+
+
+
+
+	
+	
+

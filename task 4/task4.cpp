8b8f4bf39016d2e76cc10fe5de947913fc9d4219
asrarkhan1919 <mirhadiak47@gmail.com>
2025-08-23 19:57:22 +0430

    task4

---
+++ task 4/task4.cpp
@@ -0,0 +1,31 @@
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+main(){
+
+
+ 
+  int age;
+  
+  int n=0;
+  cout<<"enter your age";
+  cin>>age;
+  while(n<10){
+  cin>>age;
+  if(age>18){
+  	
+  	cout<<"welcome to cinema"<<endl;
+  }else{
+  	cout<<"you cannot enter cinema"<<endl;
+  }
+  	 n++;
+  	 
+  
+  }
+  	
+}

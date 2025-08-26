6c267c5567638de481640cab377b4b76e33fa097
asrarkhan1919 <mirhadiak47@gmail.com>
2025-08-27 03:06:53 +0430

    hardwork

---
+++ for loops tasks/loop tasks and practes.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+int  n;
+ cout <<"enter your number";
+ cin>>n;
+ for(int m=0; m<=n; m++) {
+ 	 for(int k=1; k<=m; k++){
+	 
+ 	
+ 	cout << " * " << " * ";
+	      }cout <<" * ";
+   cout << endl; 
+ 
+  }
+	
+	
+	  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
---
+++ taskes of loops.cpp
@@ -0,0 +1,23 @@
+ #include<iostream>
+using namespace std;
+
+main(){
+	
+int o=0;
+ cin>>o;
+ for(int m=1; m<=o; m++){
+ 
+ 	for(int k=1; k<=m; k++){
+ 		
+ 		cout<<k <<" ";
+	 }
+ 	for (int k=m-1; m>=1; m--){
+ 		cout<< k <<" ";
+ 		
+	 }
+	 cout<<endl;
+ }
+ 
+
+
+}

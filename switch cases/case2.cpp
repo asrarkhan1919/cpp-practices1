4ae81880c12fe7c972223d841075767ce6511c5f
asrarkhan1919 <mirhadiak47@gmail.com>
2025-08-29 00:16:12 +0430

    switch task

---
+++ switch cases/case2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+
+//	
+//	int a;
+//	 cout<<"enter your age ";
+//	 
+// 	
+//	for (int b=1; b<=10; b++) {
+//	  cin>>a;
+//	switch (a){
+//		case 18:
+//		    cout<<"you can enter /n/n";
+//		    break;
+//		case 17:
+//			cout<<"sorry your age not proffect to enter \n\n";
+//			break;
+//		case 16:
+//		    cout <<"sorry you cant enter because your age in not good \n\n";
+//		    break;
+//		default:
+//		    cout<<"sorry only 18 age people allowed\n\n";	 
+//}
+//	  if (a>=15||a<=19){
+//	  	    cout<<"enter your age again\t: ";
+//	  	    
+//	  }else if (a>=19){cout<<"sorry your age is out of range";
+//	  }
+//		
+//	
+//		}
+//	
+	
+//	
+// int d=5;
+//	for (int a=1;  a<=d; a++){
+//		     cout <<" ";
+//		for (int b=a; b<=d; b++){
+//			cout <<b;
+//		 
+//		 	}     
+//		 cout<<"\n" ;	
+//		}
+//			 	
+//	    
+//	   
+//		
+//		
+//	}
+//		
+//		
+//		
+
+		
+//cout<<"enter numer : ";
+//		
+//int a;
+//cin>>a;
+//cout<<"enter numer : ";
+//int b;
+//cin>>b;
+//int select;
+//cout <<"what you want to do\n\n\n";
+//cout <<"1 : - \n";
+//cout <<"2 : + \n";
+//cout <<"3 : x \n";	
+//cout <<"4 : % \n";	
+//cin>> select; 
+//switch (select){
+//
+//
+//      case 1:
+//	    cout<<"= "<<a-b;
+//		break;	
+//	   case 2:
+//	    cout<<"= "<<a+b;
+//		break;	
+//	    case 3:
+//	    cout<<"= "<<a*b;
+//	    break;
+//		 case 4:
+//	    cout<<"=  "<<a/b;	
+//		break;
+//		
+//}
+//	
+//	
+//	
+//}		
+//	
+//	
+//	
+//	
+
+for (int a=1; a<=5; a++){ for (int c=1; c<=5; c++ ){ cout <<a * c <<" \t"; } cout<<endl; } }
+
+
+
+
+

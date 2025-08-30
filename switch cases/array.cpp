e0650b91f9ddf2e4191273617f03fda9185f1fac
asrarkhan1919 <mirhadiak47@gmail.com>
2025-08-30 23:34:24 +0430

    array day 2

---
+++ switch cases/array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main(){
+
+
+ 
+string players[4];
+int d;
+for(int i=0; i<=4; i++){
+	cout <<"enter name of player :-"<<i<<" :";
+	cin>>players[d];
+	
+	
+}
+
+
+for(int j=0; j<=4; j++){
+	
+	 cout<<players[d]<<endl;
+}
+
+
+
+
+
+}

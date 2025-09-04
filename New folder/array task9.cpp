3b994d3f204576dcbd3f8c1944b7fdec33e7c49a
asrarkhan1919 <mirhadiak47@gmail.com>
2025-09-05 03:54:08 +0430

    array task 9

---
+++ New folder/array task9.cpp
@@ -0,0 +1,198 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+
+
+
+//for (int r=0; r<3; r++){
+//	 for (int c=0; c<3; c++){
+//	 	cout<<"enter number :";
+//	 	cin>>arr[r][c];
+//	    }
+//	    cout<<endl;
+//    }
+//	 
+//	for (int r=0; r<3; r++){
+//	 for (int c=0; c<3; c++){
+//	 	cout<<arr[r][c]<<" ";
+//	 }
+//	 cout<<"\n";
+//	} 	 
+//	 
+//	 	for (int r=0; r<3; r++){
+//	 for (int c=0; c<3; c++){
+//	 	;
+//	 }
+//	 cout<<"\n";
+//	} 	 
+//
+//int ar1=0;
+//int ar2=0;
+//int ar3=0;
+//int max=0;
+//int arr[3][3]={{100,2,3},{9,80,7},{2,4,430}};
+//
+//	for(int r=0; r<3; r++){
+//		 for(int c=0; c<3; c++){
+//		 	if (r==0){ar1+=arr[r][c];
+//		 	
+//			}if (r==1){ar2+=arr[r][c];
+//		 	
+//			}if (r==2){ar3+=arr[r][c];
+//	         
+//			 }	
+//		 }
+//		  
+//	}
+//	max = ar1;
+//	if(max<ar2){
+//		max=ar2;
+//	}
+//	if(max<ar3){
+//		max=ar3;
+//	}
+//	cout<<"largest row sum: "<<max;
+//   
+//
+
+//int nu;
+//
+//cin>>nu;
+//
+//int arr[3][3]={{23,4,34},{43,234,2},{32,3,2}};
+//
+//   for (int r=0; r<3; r++){
+//   	 for(int c=0; c<3; c++){
+//   	 	 if(arr[r][c]==nu||arr[r][c]==nu){
+//			 cout<<"("<<r<<","<<c<<")"<<endl;
+//	     	}
+			
+//		}
+//   } 
+
+
+
+
+
+//
+//int nu;
+//
+//cin>>nu;
+//
+//int arr[3]={32,5,4};
+//
+//   for (int r=0; r<3; r++){
+//   	 	if(arr[r]==nu){
+//			 cout<<r;
+//	    }
+//	}
+//	
+//	
+// } 
+
+//int a1=1,a2=1,a3=1;
+//
+//int index;
+//int arr[3][3]={{2,9,3},{8,9,5},{5,9,9}};
+//
+//
+//  for (int r=0; r<3; r++){
+//  	for (int c=0; c<3; c++){
+//  		if (r==0){
+//  			a1*=arr[r][c];
+//		}
+//		if (r==1){
+//  			a2*=arr[r][c];
+//		}
+//		if (r==2){
+//  			a3*=arr[r][c];
+//		}
+//      }
+//    } 
+//		int total = a1;
+//		index=0;
+//		if(a1<a2){
+//		   total=a2;
+//		   index=1;
+//		}
+//		if(a2<a3){
+//			total=a3;
+//			index=2;
+//		}		
+//    cout<<"total is :"<<total<<endl;
+//    cout<<"max row is "<<index<<" : ";
+//    for(int i=0;i<3;i++){
+//    	cout<<arr[index][i]<<" ";
+//	}
+//    
+//       
+//    
+  
+	  
+	  
+	
+//	float sum;
+//	float avstar;
+//	int arr[8]={2,3,2,2,2,2,2,20};
+//	
+//	
+//	  for (int r=0; r<8; r++){
+//	  	    
+//	  	    sum+=arr[r];
+//	    }
+//	    avstar=sum/8;
+//	   
+//	    cout<<"total movie stars average: "<<avstar;
+	    
+	    
+//			int total = a1;
+//			index=0;
+//			if(a1<a2){
+//			   total=a2;
+//			   index=1;
+//			}
+//			if(a2<a3){
+//				total=a3;
+//				index=2;
+//			}		
+//	    cout<<"total is :"<<total<<endl;
+//	    cout<<"max row is "<<index<<" : ";
+//	    for(int i=0;i<3;i++){
+//	    	cout<<arr[index][i]<<" ";
+//		}
+	    
+	       
+	   	
+	float sum;
+	float maint;
+	float high;
+	float low;
+	
+	int arr[8]={20,45,41,66,42,24,28,19};
+	
+	        
+	  for (int r=0; r<8; r++){
+	  	      
+	  	    if (high<arr[r]){
+	  	    	high=arr[r];
+	  	    } 
+			  
+	  	    sum+=arr[r];
+    } 
+	    maint=sum/8;
+	    
+	   
+	   
+	    cout<<"high tempratures is: "<<high<<endl;
+	    cout<<"low tempratures is: "<<low<<endl;
+	    cout <<"main tempratures is :"<<maint;
+	      
+		 
+}
+		 
+		 
+		  
+  
+    

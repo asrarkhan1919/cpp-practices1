2980fc9ea6e5fc1691d7455baa1740f84114c008
asrarkhan1919 <mirhadiak47@gmail.com>
2025-09-06 13:22:39 +0430

    fun

---
+++ functions/without_R_P.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//string country;
+
+// function definition
+void afghanistan(){
+	string country;
+	
+	cout<<"welcome to afghanistan"<<endl;	
+}
+
+void india(){
+//	string country;
+	cout<<"welcome to india"<<endl;
+}
+
+
+main(){
+	string country;
+	cout<<"enter your country name:";
+	cin>>country;
+	
+	if(country=="afghanistan"){
+		afghanistan();
+		string country;
+		cout<<country;
+	}	
+	else if(country=="india"){
+		string country;
+		india();
+    }
+
+
+
+			
+}
+
+
+
+
+

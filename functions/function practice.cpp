e0479ba43237bb94399361d077b6f45e0dc144c7
asrarkhan1919 <mirhadiak47@gmail.com>
2025-09-09 01:28:48 +0430

    function ex

--- functions/function practice.cpp
+++ functions/function practice.cpp
@@ -1,65 +1,95 @@
 #include<iostream>
 using namespace std;
+//void addition();
+double multiplication(int num1,int num2);
+void printNumber(int number);
+string sayHello();
+void findstar();
+int main(){
+	
+	double result;
+	result=multiplication(10,10);
+	printNumber(result);
+	string hello;
+	hello=sayHello();
+	cout<<hello;
+	
+//	return_type function_name(parameters...){function_body}
+//	void function_name(parameters...){function_body}
+//	have_return_type function_name(){}
+//	no_return_type function_name(no_parameters){};
+	
+	}
 
-void addition();
+
+string sayHello(){
+	return "Hi there";
+}
 
 
-
-
- int main(){
- 
- 
- addition();
+double multiplication(int num1,int num2){
+  double result=num1*num2;
+  return result;
+  }
   
-}
+  void printNumber(int number){
+  	cout<<number;
+  }
 
-void addition(){
-	
-	for (int k=1; k<=100; k++ )	{
-	  string cls;
-	  int x;
-	  cout<<"enter your number :";
-	  cin>>x;
-	  int b;
-	  cout<<"enter your number :";
-	  cin>>b;
-	  float total;
-	  cout<<" :  select one from this:  [ +   -  x  /  ]  : \t";
-	  string point;
-	  cin>>point;
-	  
-	  if (point=="+"){
-	  	total=+x+b;
-	  	
-	  }else if(point=="-"){
-	  	total=+x-b;
-	  	
-	  } else if (point=="x"){
-	  	total=+x*b;
-	  	
-	  }else if(point=="/"){
-	  	total=+x+b;
-	  } 
-	  
-	 
-	  cout<<endl<<"total :"<<total<<endl<<endl;
-	  
-	  cout<<"do you want calculator again  :";
-	  cin>>cls;
-	  
-	  if (cls=="yes"){
-	  	system("CLS");
-	  	  
-	  	 }else if (cls=="no"){
-	  		break;
-		  }  
-	 
-	 
-	  }
-	 
-	
-}
+//void findstar(){
+//
+//
+//	
+//}
 
+ 
+//void addition(){
+//	
+//	for (int k=1; k<=100; k++ )	{
+//	  string cls;
+//	  int x;
+//	  cout<<"enter your number :";
+//	  cin>>x;
+//	  int b;
+//	  cout<<"enter your number :";
+//	  cin>>b;
+//	  float total;
+//	  cout<<" :  select one from this:  [ +   -  x  /  ]  : \t";
+//	  string point;
+//	  cin>>point;
+//	  
+//	  if (point=="+"){
+//	  	total=+x+b;
+//	  	
+//	  }else if(point=="-"){
+//	  	total=+x-b;
+//	  	
+//	  } else if (point=="x"){
+//	  	total=+x*b;
+//	  	
+//	  }else if(point=="/"){
+//	  	total=+x+b;
+//	  } 
+//	  
+//	 
+//	  cout<<endl<<"total :"<<total<<endl<<endl;
+//	  
+//	  cout<<"do you want calculator again  :";
+//	  cin>>cls;
+//	  
+//	  if (cls=="yes"){
+//	  	system("CLS");
+//	  	  
+//	  	 }else if (cls=="no"){
+//	  		break;
+//		  }  
+//	 
+//	 
+//	  }
+//	 
+//	
+//}
+//
 
 
 
